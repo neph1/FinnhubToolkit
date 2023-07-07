c7a279d5ee4f530bb764670ebeeeaa007ffba2bd
rickard <rickardeden@gmail.com>
2023-07-07 16:50:00 +0200

    initial commit

---
+++ finnhub_toolkit.py
@@ -0,0 +1,20 @@
+from abc import ABC
+from typing import List
+
+from superagi.tools.base_tool import BaseTool, BaseToolkit
+from finnhub_basic_financials import FinnhubBasicFinancialsTool
+from finnhub_candles import FinnhubCandlesTool
+from finnhub_company_news import FinnhubCompanyNewsTool
+from finnhub_market_news import FinnhubMarketNewsTool
+from finnhub_quote import FinnhubQuoteTool
+from finnhub_technical_indicators import FinnhubTechnicalIndicatorsTool
+
+class FinnhubToolkit():
+    name: str = "FinnhubToolkit"
+    description: "Toolkit that contains various tools to interact with Finnhubs api"
+
+    def get_tools(self) -> List[BaseTool]:
+        return [FinnhubCompanyNewsTool(), FinnhubCandlesTool(), FinnhubBasicFinancialsTool(), FinnhubTechnicalIndicatorsTool(), FinnhubQuoteTool(), FinnhubMarketNewsTool()]
+
+    def get_env_keys(self) -> List[str]:
+        return ["FINNHUB_API_KEY"]
